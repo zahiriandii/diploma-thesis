f532b651c466315a640a795e33d2dfe08261f1dc
AndiZahiri <zahiriandii@gmail.com>
2025-04-09 16:38:29 +0200

    Initial init

---
+++ src/Modals/CityModal.vue
@@ -0,0 +1,56 @@
+<template>
+  <Page>
+    
+    <GridLayout rows="auto, *" columns="auto, *">
+      
+      <!-- Back Button (Row 0, Column 0) -->
+      <Button
+        text="Back"
+        @tap="$modal.close()"
+        row="0"
+        col="1"
+        backgroundColor="#007aff"
+        color="white"
+        borderRadius="5"
+      />
+
+     
+      <Label text="City Selection" row="1" col="1" verticalAlignment="center" />
+      
+      
+      <StackLayout row="2" col="0" colSpan="2">
+        <Label text="Choose a city" />
+        <ListView :items="cities" @itemTap="selectDesiredCity">
+          <template #item="{ item }">
+            <StackLayout>
+              <Label :text="item" />
+            </StackLayout>
+        </template>
+      </ListView>
+      </StackLayout>
+      
+
+    </GridLayout>
+    
+  </Page>
+</template>
+
+<script lang="ts" setup>
+import { $closeModal } from 'nativescript-vue';
+
+  
+  
+ const props = defineProps<{
+  cities: string[] 
+ }>()
+ 
+ const selectDesiredCity = (args: any) => {
+  const index = args.index;  // Get the index of the selected item
+  const city = props.cities[index];  // Access the selected city from the cities array
+  console.log('Selected city:', city);
+  $closeModal(city);  // Close modal and return selected city
+
+};
+
+  
+</script>
---
+++ src/components/Home.vue
@@ -0,0 +1,66 @@
+<script lang="ts" setup>
+
+  import { $showModal, ref } from 'nativescript-vue';
+import CityModal from '~/Modals/CityModal.vue';
+
+  const selectedCity = ref('');
+
+   const selectCity = () =>
+   {
+    $showModal(CityModal, {
+      fullscreen: true,
+      props: {
+        cities: ["Kumanovo","Skopje","Wien"] 
+      },
+      closeCallback: (selected) =>
+      {
+        selectedCity.value = selected;
+      }
+    });
+   }
+
+</script>
+
+<template>
+  
+  <Page>
+    <StackLayout>
+      <!-- Custom "ActionBar" -->
+      <GridLayout columns="auto, *" rows="auto" class="custom-action-bar">
+        <Label text="🏠" col="0" class="icon" />
+        <Label text="Home Component" col="1" class="title" />
+      </GridLayout>
+
+      <!-- Page content -->
+      <StackLayout>
+        <Label text="From" />
+        <Button text="Select a City" @tap="selectCity" />
+        <Label :text="'Selected City is ' + selectedCity" />
+      </StackLayout>
+    </StackLayout>
+  </Page>
+
+</template>
+
+<style>
+.custom-action-bar {
+  padding: 12px;
+  background-color: #3f51b5;
+  color: white;
+  height: 56;
+  vertical-align: middle;
+}
+
+.title {
+  font-size: 20;
+  color: white;
+  margin-left: 12;
+  vertical-align: center;
+}
+
+.icon {
+  font-size: 20;
+  color: white;
+  vertical-align: center;
+}
+</style>
