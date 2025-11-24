3e3537e4ec5670d5c23bb1f984c46d7bd58fca3d
Andi Zahiri <zahiriandii@gmail.com>
2025-11-24 17:17:12 +0100

    Added the function to register a user on the DB

--- src/Modals/RegisterModal.vue
+++ src/Modals/RegisterModal.vue
@@ -48,7 +48,7 @@
         <TextField v-model="countryCode" hint="Country code" class="border border-gray-300 rounded-lg px-4 py-2 text-base" />
 
         <!-- Mobile Phone -->
-        <TextField v-model="phone" hint="Mobile Phone number" keyboardType="phone" class="border border-gray-300 rounded-lg px-4 py-2 text-base" />
+        <TextField v-model="phoneNumber" hint="Mobile Phone number" keyboardType="phone" class="border border-gray-300 rounded-lg px-4 py-2 text-base" />
 
         <!-- E-mail -->
         <TextField v-model="email" hint="E-mail" keyboardType="email" autocorrect="false" autocapitalizationType="none" class="border border-gray-300 rounded-lg px-4 py-2 text-base" />
@@ -72,18 +72,23 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'nativescript-vue';
+import { alert, ApplicationSettings,exitEvent } from '@nativescript/core';
+import { $closeModal, ref } from 'nativescript-vue';
+import { json } from 'stream/consumers';
 
 const firstName = ref('');
 const lastName = ref('');
 const birthDate = ref('');
 const gender = ref('');
 const countryCode = ref('');
-const phone = ref('');
+const phoneNumber = ref('');
 const email = ref('');
 const password = ref('');
 const country = ref('');
 const showPassword = ref(false);
+const isLoading = ref(false);
+
+const base_backend_url = 'http://10.0.2.2:8080';
 
 function togglePassword() {
   showPassword.value = !showPassword.value;
@@ -93,13 +98,75 @@ function selectGender(value: string) {
   gender.value = value;
 }
 
-function register() {
-  console.log('Register pressed', {
-    firstName: firstName.value,
-    lastName: lastName.value,
-    gender: gender.value,
-    email: email.value,
-  });
+const register =  async () => {
+  
+  if (!firstName.value || !lastName.value || !birthDate.value || !gender.value || !countryCode.value || !phoneNumber.value || !email.value || !password.value || !country.value )
+    {
+      alert("Please fill the required fields in order to continue to register.");
+      return;
+    }
+
+    try {
+      isLoading.value = true;
+
+      const response = await fetch(`${base_backend_url}/auth/register`,
+        {
+          method: 'POST',
+          headers:
+          {
+            'Content-type' : 'application/json'
+          },
+          body: JSON.stringify({
+            firstName: firstName.value,
+            lastName: lastName.value,
+            email: email.value,
+            password: password.value,
+            phoneNumber: phoneNumber.value,
+            gender: gender.value.toUpperCase(),
+            birthDate: birthDate.value,
+            country: country.value
+          }),
+          
+        }
+      );
+
+        if (!response.ok)
+        {
+          const errotText = await response.text();
+          console.log("Error text: ",errotText);
+          alert("Registration failed , please try again later!");
+          return;
+        }
+        
+
+        const data = await response.json();
+        console.log("Registration is successful");
+        if (data.token)
+        {
+          ApplicationSettings.setString("authToken", data.token);
+          ApplicationSettings.setString("userId", data.userId.toString());
+          ApplicationSettings.setString("userEmail", data.email);
+          ApplicationSettings.setString("firstName", data.firstName);
+          ApplicationSettings.setString("lastName", data.lastName);
+          console.log(data.firstName)
+
+          $closeModal({success : true});
+        } 
+        //closeModal after successful registration
+        $closeModal();
+
+      
+    } catch (error) {
+      
+      console.log("Error while registering ,", error);
+      alert("Something went wrong , please try again later");
+    }
+    finally
+    {
+      isLoading.value=false;
+    }
+
+
 }
 
 function close() {
