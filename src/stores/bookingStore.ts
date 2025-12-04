decbe950fb78cc286805760daa32ba5b9825b79f
Andi Zahiri <zahiriandii@gmail.com>
2025-12-04 18:54:16 +0100

    Modified the bookingStore.ts to calculate the total price and to store the passengerForm

--- src/stores/bookingStore.ts
+++ src/stores/bookingStore.ts
@@ -1,16 +1,25 @@
 import { reactive, computed } from "nativescript-vue";
 
+export type passengerForm = {
+  firstName: string;
+  lastName: string;
+  type: 'ADULT' | 'CHILD' | 'INFANT';
+  seatId?: number;
+  seatNumber?: string;
+};
+
 export const bookingState = reactive({
   tripId: null as number | null,
   departureStation: "",
   arrivalStation: "",
-  basePrice: 0,
+  basePrice: 100,
 
   selectedPassengers: {
-    adults: 0,
+    adults: 1,
     children: 0,
     infants: 0
   },
+  passengersForm: [] as passengerForm[],
 
   selectedSeats: [] as { seatId: number; seatNumber: string }[],
 
@@ -18,6 +27,7 @@ export const bookingState = reactive({
     extraLuggageCount: 0
   }
 });
+
 
 export const totalPassengers = computed(() =>
   bookingState.selectedPassengers.adults +
@@ -27,6 +37,6 @@ export const totalPassengers = computed(() =>
 
 export const totalPrice = computed(() => {
   const base = bookingState.basePrice * totalPassengers.value;
-  const extraLuggage = bookingState.extras.extraLuggageCount * 160; // example
+  const extraLuggage = bookingState.extras.extraLuggageCount * 3; // example
   return base + extraLuggage;
 });
