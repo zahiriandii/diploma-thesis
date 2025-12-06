22645bcce2fb1d11a5c21d898decfc998fcb4fa1
Andi Zahiri <zahiriandii@gmail.com>
2025-12-06 14:20:43 +0100

    Creted the Booking successful modal , to return to the home screen and to inform the user for the booking

---
+++ src/Modals/BookingSuccessfulModal.vue
@@ -0,0 +1,26 @@
+<template>
+  <Page>
+
+      <StackLayout class="p-6 items-center space-y-4">
+        <Label text="🎉 Booking Successful!" class="text-xl font-bold text-green-600" />
+
+        <Button
+          text="Go to Home"
+          class="bg-lime-500 text-white rounded-xl px-6 py-2"
+          @tap="goHome"
+        />
+      </StackLayout>
+   
+  </Page>
+</template>
+
+<script setup lang="ts">
+import Home from '~/components/Home.vue';
+import { $navigateTo } from 'nativescript-vue';
+
+const goHome = () => {
+  $navigateTo(Home, {
+    clearHistory: true,
+  });
+};
+</script>
