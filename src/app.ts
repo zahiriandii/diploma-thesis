2f27a52574be74c8ed8f917f11a5a13a26d71afb
Andi Zahiri <zahiriandii@gmail.com>
2025-12-08 15:36:38 +0100

    Installed the Stripe plugin and modified the app.ts to use the CreditCardView

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
-import { createApp } from 'nativescript-vue';
+import { createApp, registerElement } from 'nativescript-vue';
 import AppRoot from './components/AppRoot.vue';
 import './app.css';
 import RadCalendar from 'nativescript-ui-calendar/vue';
+import { CreditCardView } from '@triniwiz/nativescript-stripe';
 
+registerElement('CreditCardView', () => CreditCardView);
 
 const app = createApp(AppRoot);
+
 app.use(RadCalendar);
+
 app.start();
